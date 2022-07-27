acf5660e8885c9458fb591cfa824646d0716a446
ishwar kumar <ishwar2510@gmail.com>
2022-07-27 20:46:17 +0530

    Functionality pending

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import react,{useState} from 'react'
-import { Button,Card, CardBody } from 'reactstrap';
+import { Button,Card, CardBody ,Container} from 'reactstrap';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Icon from './Icon';
@@ -9,20 +9,40 @@ const itemArray=new Array(9).fill('empty');
 function App() {
   const [isCross,setCross]=useState(false);
   const [winMsg,setWiner]=useState(0);
+  function changeItem(itemNumber){
+     
+    if(winMsg){
+      return toast(winMsg,{type:"success"})
+    }
+    if(itemArray[itemNumber]==="empty"){
+      itemArray[itemNumber]= isCross? "cross":'circle';
+      setCross(!isCross);
+    }else{
+      return toast("already set",{type:'error'});
+    }
+
+  }
+  const reloadGame=()=>{
+    setCross(false);
+    setWiner("");
+    itemArray.fill("empty");
+
+  };
   
   return (
     <>
+    <Container>
+    <ToastContainer position='bottom-center'/>
       {winMsg
       ?(
         <div>
-        {winMsg}
-        <Button > Reset </Button>
+          <h1 className='heading'>  {winMsg} </h1>
+        <Button  color='success' onClick={reloadGame()}> Reset </Button>
         </div>
       )
       : (
         <div>
-          {isCross
-          ?("circle"):("cross") }     turns
+         <h1 className='heading'> {isCross ? ("cross") : ("circle")  }     turns</h1>
         </div>
       )
       }
@@ -31,7 +51,7 @@ function App() {
      {
       itemArray.map((item,index)=>{
         return (
-          <Card color='warning'>
+          <Card color='warning' onClick={()=>{changeItem(index)}}>
           <CardBody className='box'>
           <Icon player={item}/>
           </CardBody>
@@ -43,7 +63,7 @@ function App() {
 
      </div>
     
-    
+     </Container>
 
     </>
   );
